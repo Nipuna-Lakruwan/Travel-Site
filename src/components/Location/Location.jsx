e1006d43640a0c037ce1ccf5a98efad37e89eb42
Nipuna Lakruwan <senarath.lakruwan@gmail.com>
2024-07-14 03:02:23 +0530

    Update Location.jsx

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -12,7 +12,7 @@ const Location = () => {
 
           <div className="rounded-xl ">
             <iframe
-              src="https://www.google.com/maps/search/places+to+visit+sri+lanka/@7.8494358,78.0640793,7z/data=!3m1!4b1?entry=ttu"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4047293.6600842737!2d78.06407926329861!3d7.849435767178971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3662db149fbf5%3A0x8165d70ac115e887!2sSri%20Dalada%20Maligawa!5e0!3m2!1sen!2slk!4v1720906118185!5m2!1sen!2slk"
               width="100%"
               height="360"
               allowfullscreen=""
